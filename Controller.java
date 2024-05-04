d04627186d8837c33c5385970eb8bc66caa9d7e9
Revwali <92225427+Revwali@users.noreply.github.com>
2024-05-05 00:50:40 +0530

    formatted code for controller class

--- Controller.java
+++ Controller.java
@@ -10,6 +10,7 @@ import org.apache.tomcat.util.json.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JsonParser;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -24,30 +25,30 @@ import com.sun.net.httpserver.Authenticator.Result;
 
 @RestController()
 public class Controller {
-	
-//	@Autowired
-	private RestTemplate restTemplate=new RestTemplate();
 
-	@GetMapping("/g")
-	public News sayHi() throws JsonMappingException, JsonProcessingException, ParseException {
-		Map<String,ArrayList<Map<String,String>>> res = new HashMap<String, ArrayList<Map<String,String>>>();
-		 ResponseEntity<Map> s= restTemplate.getForEntity(
-		  "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=udAHowoeyoAWtCtKiVEdEp8E0UYKHQ7D",
-		  Map.class);
-		 res =s.getBody(); 
-		 News news=new News();
-		 Results[] results=new Results[3];
-		 results[0]=new Results();
-		 results[0].setTitle(res.get("results").get(0).get("title"));
-		  news.setResults(results);
-		 news.setSection(res.get("results").get(0).get("section"));
-		 news.setTitle(res.get("results").get(0).get("title"));
-		 return news;
-		 
-		 
-			 
-		
-		
-		 		
-	}
+    //	@Autowired
+    private RestTemplate restTemplate = new RestTemplate();
+
+    // @CrossOrigin(origins = "*")
+    @GetMapping("/g")
+    public News sayHi() throws JsonMappingException, JsonProcessingException, ParseException {
+        Map<String, ArrayList<Map<String, String>>> res = new HashMap<String, ArrayList<Map<String, String>>>();
+        ResponseEntity<Map> s = restTemplate.getForEntity(
+                "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=udAHowoeyoAWtCtKiVEdEp8E0UYKHQ7D",
+                Map.class);
+
+        res = s.getBody();
+
+        News news = new News();
+        Results[] results = new Results[3];
+        results[0] = new
+                Results();
+        results[0].setTitle(res.get("results").get(0).get("title"));
+        //  news.setResults(results);
+        news.setSection(res.get("results").get(0).get("section"));
+        news.setTitle(res.get("results").get(0).get("title"));
+        return news;
+
+
+    }
 }
